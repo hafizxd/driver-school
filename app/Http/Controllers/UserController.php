74817bbc38565ef20520d7605ce2676ef91f71f7
reiyanyan <hariyantoriyan.hr@gmail.com>
2019-01-21 22:11:44 +0700

    fix typo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     
     public function store(Request $request){
         $user = new User;
-        $user->name = $request->email;
+        $user->name = $request->name;
         $user->email = $request->email;
         $user->password = $request->password;
         $user->save();
